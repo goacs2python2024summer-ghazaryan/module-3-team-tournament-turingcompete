358055e279413eb6a1ec7992aaa42cf5f7cefdee
Mora32 <parkdogg808@gmail.com>
2024-07-05 13:30:32 -1000

    made 2nd function for my team

--- groupstrategies.py
+++ groupstrategies.py
@@ -5,7 +5,7 @@
 # Here are some history variables to test your code on. Feel free to create your own.
 hist1 = []
 hist2 = [("split","steal")]
-hist3 = [("split","split"),("steal","split"),("split,steal"),("split,split"),("steal,split")]
+hist3 = [("split","split"),("steal","split"),("split","steal"),("split","split"),("steal","split")]
 hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split"),("split","split"),("steal","split")]
 
 # Your team's 1st strategy (leave blank if you are not the driver)
@@ -14,10 +14,21 @@ hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split")
 
 
 # Your team's 2nd strategy (leave blank if you are not the driver)
-# Explanation of Strategy:
+# Explanation of Strategy: output is the opposite of the last choice of the opponent
 # 
+def turingcompetestrategy2(hist):
+    try:
+        last = hist[len(hist) - 1]
+        if last[1] == "split":
+            return "steal"
+        else:
+            return "split"
+    except IndexError:
+        return "split"
+
 
 
 # Your team's 3rd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
 # 
+print(turingcompetestrategy2(hist1))
